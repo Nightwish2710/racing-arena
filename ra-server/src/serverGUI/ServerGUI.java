7431c35ee7fd78fe2cf995e9252868bd8fe1be14
Nightwish2710 <45417031+Nightwish2710@users.noreply.github.com>
2020-11-26 17:24:47 +0700

    update Racer Stat table

--- ra-server/src/serverGUI/ServerGUI.java
+++ ra-server/src/serverGUI/ServerGUI.java
@@ -1,12 +1,15 @@
 package serverGUI;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.*;
+import javax.swing.border.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ServerGUI extends JFrame {
-    private static String[] tableColumns = {"Order", "Racer", "Gain", "Status", "Position"};
-
     private JPanel ServerPanel;
 
     private JPanel serverLogsPanel;
@@ -26,9 +29,11 @@ public class ServerGUI extends JFrame {
     private JLabel numOfPplJoiningLabel;
     private JLabel numOfPplJoining;
 
-    private JSeparator separator1;
+    private JSeparator separator1, separator2;
 
-    private JTable racerStatusTable;
+    public JTable racerStatTable;
+    private JLabel racerStatLabel;
+    private JScrollPane statTableScrollPane;
 
     public ServerGUI(String gameName) {
         super(gameName);
@@ -52,11 +57,27 @@ public class ServerGUI extends JFrame {
         gameConfigLabel.setFont(new Font("Britannic Bold", Font.PLAIN, 16));
         gameControlLabel.setFont(new Font("Britannic Bold", Font.PLAIN, 16));
 
-        serverLogsPanelLabel.setForeground(ServerGUIConfig.ACCENT_COLOR);
         serverLogsPanelLabel.setFont(new Font("Britannic Bold", Font.PLAIN, 20));
+        racerStatLabel.setFont(new Font("Britannic Bold", Font.PLAIN, 20));
 
         openConnectionWarning.setFont(new Font("Arial", Font.ITALIC, 9));
         openConnectionWarning.setForeground(Color.RED);
+
+        // set button
+        openConnectionButton.setBackground(ServerGUIConfig.LIGHT_GREEN);
+        openConnectionButton.setBorder(new LineBorder(ServerGUIConfig.LIGHT_GREEN));
+
+        startGameButton.setBackground(ServerGUIConfig.LIGHT_GREEN);
+        startGameButton.setBorder(new LineBorder(ServerGUIConfig.LIGHT_GREEN));
+
+        // set separator
+        separator1.setBackground(ServerGUIConfig.BORDER_COLOR);
+        separator1.setForeground(ServerGUIConfig.BORDER_COLOR);
+        separator1.setBorder(BorderFactory.createMatteBorder(3, 0, 0, 0, ServerGUIConfig.BORDER_COLOR));
+
+        separator2.setBackground(ServerGUIConfig.BORDER_COLOR);
+        separator2.setForeground(ServerGUIConfig.BORDER_COLOR);
+        separator2.setBorder(BorderFactory.createMatteBorder(3, 0, 0, 0, ServerGUIConfig.BORDER_COLOR));
 
         // set table
         setTable();
@@ -67,12 +88,110 @@ public class ServerGUI extends JFrame {
     }
 
     private void setTable() {
-        DefaultTableModel dtm = new DefaultTableModel(null, tableColumns) {
+        // set scroll pane
+        statTableScrollPane.setViewportBorder(null);
+        statTableScrollPane.getVerticalScrollBar().setBorder(null);
+        statTableScrollPane.getHorizontalScrollBar().setBorder(null);
+        statTableScrollPane.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+
+        // set table
+        createUIComponents();
+    }
+
+    // create table UI
+    // this is a default function, therefore, have to use this name for the function
+    private void createUIComponents() {
+        // create table
+        DefaultTableModel dtm = new DefaultTableModel(null, ServerGUIConfig.TABLE_COLS) {
             @Override
-            public Class<?> getColumnClass(int col) {
-                return getValueAt(0, col).getClass();
+            public Class<?> getColumnClass(int column) {
+                switch (column) {
+                    case 0:
+                    case 1:
+                        return Integer.class;
+                    case 2:
+                        return ImageIcon.class;
+                    default:
+                        return Object.class;
+                }
             }
         };
-        JTable racerStatusTable = new JTable(dtm);
+        dtm.setColumnIdentifiers(ServerGUIConfig.TABLE_COLS);
+        racerStatTable = new JTable(dtm) {
+            // return column class
+            @Override
+            public Class getColumnClass(int column) {
+                return getValueAt(0, column).getClass();
+            }
+            // turn off cell modification
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return true;
+            }
+            // interchange background color for each row
+            @Override
+            public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+                Component c = super.prepareRenderer(renderer, row, column);
+                if (!c.getBackground().equals(getSelectionBackground())){
+                    Color bgColor = (row % 2 == 0 ? Color.WHITE : ServerGUIConfig.BORDER_COLOR);
+                    c .setBackground(bgColor);
+                    bgColor = null;
+                }
+                return c;
+            }
+        };
+
+        // set table: row height, empty border, no horizontal line
+        racerStatTable.setRowHeight(ServerGUIConfig.ROW_HEIGHT);
+        racerStatTable.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+        racerStatTable.setShowHorizontalLines(false);
+
+        // set table's header: no border, row height, bg color, text color, text font
+        UIManager.getDefaults().put("TableHeader.cellBorder", BorderFactory.createEmptyBorder(0,0,0,0));
+        racerStatTable.getTableHeader().setPreferredSize(new Dimension(-1, ServerGUIConfig.ROW_HEIGHT-2));
+        racerStatTable.getTableHeader().setBackground(Color.BLACK);
+        racerStatTable.getTableHeader().setForeground(Color.WHITE);
+        racerStatTable.getTableHeader().setFont(new Font("Britannic Bold", Font.PLAIN, 12));
+
+        // set column width
+        racerStatTable.getColumnModel().getColumn(0).setMaxWidth(50);
+        racerStatTable.getColumnModel().getColumn(1).setMaxWidth(110);
+        racerStatTable.getColumnModel().getColumn(2).setMaxWidth(50);
+        racerStatTable.getColumnModel().getColumn(3).setMaxWidth(90);
+        racerStatTable.getColumnModel().getColumn(4).setMaxWidth(70);
+
+        // align center text in each cell
+        DefaultTableCellRenderer center = new DefaultTableCellRenderer();
+        center.setHorizontalAlignment(JLabel.CENTER);
+
+        racerStatTable.getColumnModel().getColumn(0).setCellRenderer(center);
+        racerStatTable.getColumnModel().getColumn(1).setCellRenderer(center);
+        racerStatTable.getColumnModel().getColumn(2).setCellRenderer(center);
+        racerStatTable.getColumnModel().getColumn(3).setCellRenderer(center);
+        racerStatTable.getColumnModel().getColumn(4).setCellRenderer(center);
+
+//        BufferedImage winningCupIcon = null;
+//        JLabel picLabel = null;
+//        try {
+//            winningCupIcon = ImageIO.read(this.getClass().getResource("/winner-cup.jpg"));
+//            winningCupIcon.getScaledInstance(15, 15, Image.SCALE_DEFAULT);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        JLabel winningCupIcon = new JLabel(new ImageIcon("winner-cup.jpg"));
+
+        dtm.addRow(new Object[]{1,"HHHHHHHHHH", "+10", "ELIMINATED", 10});
+        dtm.addRow(new Object[]{2, "HHHHHHHHHH", "+10", "ELIMINATED", 10});
+        dtm.addRow(new Object[]{3, "HHHHHHHHHH", "+10", "ELIMINATED", 10});
+        dtm.addRow(new Object[]{4, "HHHHHHHHHH", "+10", "ELIMINATED", 10});
+        dtm.addRow(new Object[]{5, "HHHHHHHHHH", "+10", "ELIMINATED", 10});
+        dtm.addRow(new Object[]{6, "HHHHHHHHHH", "+10", "ELIMINATED", 10});
+        dtm.addRow(new Object[]{7, "HHHHHHHHHH", "+10", "ELIMINATED", 10});
+        dtm.addRow(new Object[]{8, "HHHHHHHHHH", "+10", "ELIMINATED", 10});
+        dtm.addRow(new Object[]{9, "HHHHHHHHHH", "+10", "ELIMINATED", 10});
+        dtm.addRow(new Object[]{10, "HHHHHHHHHH", "+10", "ELIMINATED", 10});
+
+//        dtm.setValueAt(winningCupIcon, 0, 0);
     }
 }
--- ra-server/src/serverGUI/ServerGUIConfig.java
+++ ra-server/src/serverGUI/ServerGUIConfig.java
@@ -1,9 +1,17 @@
 package serverGUI;
 
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class ServerGUIConfig {
     public static final String GAME_NAME = "RACING ARENA: Server";
+
+    public static final int NUM_FIELD = 5;
+    public static final int ROW_HEIGHT = 25;
+    public static final String[] TABLE_COLS = {"ORDER", "RACER NAME", "GAIN", "STATUS", "POSITION"};
 
     public static final int MIN_NUM_OF_RACER = 2;
     public static final int MAX_NUM_OF_RACER = 10;
@@ -12,6 +20,13 @@ public class ServerGUIConfig {
     public static final int MAX_RACE_LENGTH = 26;
 
     public static final Color BACKGROUND_COLOR = new Color(0xF5F5F5);
-    public static final Color BORDER_COLOR = new Color(0xB0BEC5);
-    public static final Color ACCENT_COLOR = new Color(0x808E95);
+    public static final Color BORDER_COLOR = new Color(0xE0E0E0);
+    public static final Color LIGHT_GREEN = new Color(0x9CCC65);
+
+    public static final Color ELIMINATED_COLOR = new Color(0xEF5350);
+    public static final Color WARNING_COLOR = new Color(0xFFA726);
+    public static final Color CORRECT_COLOR = new Color(0x66BB6A);
+
+//    public static final Icon WINNING_ICON = new ImageIcon("../res/winning-cup.png");
+    public static final String WINNING_ICON = "winning-cup.png";
 }
