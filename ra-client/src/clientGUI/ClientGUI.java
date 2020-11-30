36b105c86c77a3c51dcef68fd4975e30f42dd2d3
Nightwish2710 <45417031+Nightwish2710@users.noreply.github.com>
2020-11-30 21:33:46 +0700

    update question

--- ra-client/src/clientGUI/ClientGUI.java
+++ ra-client/src/clientGUI/ClientGUI.java
@@ -65,6 +65,7 @@ public class ClientGUI extends JFrame {
     private JTextArea consoleTextArea;
 
     private JButton c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15;
+    private JLabel questionWarn;
 
     // error pane components
     JOptionPane noOpenConnectionPane;
