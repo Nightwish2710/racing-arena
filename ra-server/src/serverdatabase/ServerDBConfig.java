fcd1d51aba9eed791f383a0e87f14334c865f301
Nightwish2710 <45417031+Nightwish2710@users.noreply.github.com>
2020-12-02 21:40:39 +0700

    finalize

--- ra-server/src/serverdatabase/ServerDBConfig.java
+++ ra-server/src/serverdatabase/ServerDBConfig.java
@@ -3,14 +3,14 @@ package serverdatabase;
 public class ServerDBConfig {
     // JDBC H2 driver name and database URL
     public static final String JDBC_DRIVER_H2 = "org.h2.Driver";
-    public static final String DB_URL_H2 = "jdbc:h2:~/Racing Arena/ra-test";
+    public static final String DB_URL_H2 = "jdbc:h2:~/Racing Arena/racing-arena-db";
 
     // database credentials
     public static final String DB_USER_H2 = "sa";
     public static final String DB_PASS_H2 = " ";
 
     // database constant
-    public static final String TABLE_RACER = "RACER_ACCOUNT_TEST";
+    public static final String TABLE_RACER = "RACER_ACCOUNT";
     public static final String TABLE_RACER_username = "username";
     public static final String TABLE_RACER_password = "password";
     public static final String TABLE_RACER_isonline = "isonline";
